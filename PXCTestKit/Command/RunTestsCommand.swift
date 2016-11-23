112df5819061257a08a67b9ca32ba69ec9c28bf2
Johannes Plunien <plu@pqpq.de>
2016-11-23 13:58:37 +0400

    Run tests

--- PXCTestKit/Command/RunTestsCommand.swift
+++ PXCTestKit/Command/RunTestsCommand.swift
@@ -26,6 +26,52 @@ final class RunTestsCommand {
     }
 
     func run() throws {
+        try XCTestBootstrapFrameworkLoader.loadPrivateFrameworks(nil)
+
+        let testRun = try FBXCTestRun.withTestRunFile(atPath: configuration.testRun.path).build()
+        let application = try FBApplicationDescriptor.application(withPath: testRun.testHostPath)
+
+        let simulatorControlConfiguration = FBSimulatorControlConfiguration(deviceSetPath: configuration.deviceSet.path, options: [])
+        let control = try FBSimulatorControl.withConfiguration(simulatorControlConfiguration)
+
+        let simulators = try configuration.simulators.map {
+            try control.pool.allocateSimulator(with: $0, options: [.create, .reuse])
+        }
+
+        for simulator in simulators {
+            if simulator.state == .booted {
+                continue
+            }
+            try simulator.interact
+                .bootSimulator()
+                .perform()
+        }
+
+        let applicationLaunchConfiguration = FBApplicationLaunchConfiguration(
+            application: application,
+            arguments: testRun.arguments,
+            environment: testRun.environment,
+            options: []
+        )
+
+        let testLaunchConfiguration = FBTestLaunchConfiguration(testBundlePath: testRun.testBundlePath)
+            .withApplicationLaunchConfiguration(applicationLaunchConfiguration)
+            .withTimeout(configuration.timeout)
+            .withTestsToSkip(testRun.testsToSkip)
+            .withTestsToRun(testRun.testsToRun)
+
+        for simulator in simulators {
+            try simulator.interact
+                .installApplication(application)
+                .startTest(with: testLaunchConfiguration)
+                .perform()
+        }
+
+        for simulator in simulators {
+            try simulator.interact
+                .waitUntilAllTestRunnersHaveFinishedTesting(withTimeout: configuration.timeout)
+                .perform()
+        }
     }
 
 }
