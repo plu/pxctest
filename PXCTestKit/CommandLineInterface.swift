a8a88bff2f752e564b4823628d67ef1f837af522
Johannes Plunien <plu@pqpq.de>
2016-11-23 13:26:50 +0400

    Fix option name

--- PXCTestKit/CommandLineInterface.swift
+++ PXCTestKit/CommandLineInterface.swift
@@ -18,7 +18,7 @@ import FBSimulatorControl
                        Option<ExistingFileURL>("xctestrun", ExistingFileURL(url: URL(fileURLWithPath: "")), description: "Path to the .xctestrun file."),
                        Option<ExistingFileURL>("deviceset", ExistingFileURL(url: URL(fileURLWithPath: FBSimulatorControlConfiguration.defaultDeviceSetPath())), description: "Path to the Simulator device set."),
                        Option<FileURL>("output", FileURL(url: URL(fileURLWithPath: "test-reports")), description: "Path where the test output should be written to."),
-                       VaradicOption<Destination>("output", [], description: "A comma-separated set of key=value pairs describing the destination to use, just like xcodebuild -destination."),
+                       VaradicOption<Destination>("destination", [], description: "A comma-separated set of key=value pairs describing the destination to use, just like xcodebuild -destination."),
                        Option<Double>("timeout", 3600.0, description: "Timeout in seconds for the test execution to finish.")
             ) {
                 let configuration = RunTestsCommand.Configuration(
