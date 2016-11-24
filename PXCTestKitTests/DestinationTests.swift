66f25517fc01a817feefe533b6e9865bff5334e4
Johannes Plunien <plu@pqpq.de>
2016-11-24 14:38:32 +0400

    Extend DestinationTests

--- PXCTestKitTests/DestinationTests.swift
+++ PXCTestKitTests/DestinationTests.swift
@@ -12,10 +12,34 @@ import XCTest
 
 class DestinationTests: XCTestCase {
 
-    func testParsingDestination() throws {
+    func testParsingValidDestination() throws {
         let destination = try Destination(destinationIdentifier: "os=iOS 9.1,name=iPhone 5")
         XCTAssertEqual(destination.simulatorConfiguration.osVersionString, "iOS 9.1")
         XCTAssertEqual(destination.simulatorConfiguration.deviceName, "iPhone 5")
+    }
+
+    func testParsingDestinationWithoutOS() throws {
+        let destination = try Destination(destinationIdentifier: "name=iPhone 5")
+        XCTAssertEqual(destination.simulatorConfiguration.osVersionString, "iOS 10.1")
+        XCTAssertEqual(destination.simulatorConfiguration.deviceName, "iPhone 5")
+    }
+
+    func testParsingDestinationWithoutDevice() throws {
+        let destination = try Destination(destinationIdentifier: "os=iOS 9.1")
+        XCTAssertEqual(destination.simulatorConfiguration.osVersionString, "iOS 9.1")
+        XCTAssertEqual(destination.simulatorConfiguration.deviceName, "iPhone 6")
+    }
+
+    func testParsingInvalidDestination() throws {
+        XCTAssertThrowsError(try Destination(destinationIdentifier: "some=thing"))
+    }
+
+    func testParsingInvalidDeviceName() {
+        XCTAssertThrowsError(try Destination(destinationIdentifier: "os=iOS 9.1,name=iPhone 1"))
+    }
+
+    func testParsingInvalidOSName() {
+        XCTAssertThrowsError(try Destination(destinationIdentifier: "os=iOS 2.0,name=iPhone 5"))
     }
 
 }
