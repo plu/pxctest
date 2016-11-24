0405b78a02624f8a311a7fb8a205300e12a1c875
Johannes Plunien <plu@pqpq.de>
2016-11-24 07:26:28 +0400

    Add build script

---
+++ scripts/build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+DESTINATION=$1
+PRODUCTS=$PWD/build/Products
+CONFIGURATION=Release
+
+if [ -z "$DESTINATION" ]; then
+    echo "Usage: scripts/build.sh destination"
+    exit 1
+fi
+
+xcodebuild \
+  -IDEBuildLocationStyle=Custom \
+  -IDECustomBuildLocationType=Absolute \
+  -IDECustomBuildProductsPath="$PRODUCTS" \
+  -configuration $CONFIGURATION \
+  -scheme pxctest \
+  -project pxctest.xcodeproj
+
+mkdir -p "$DESTINATION"
+
+mv $PRODUCTS/$CONFIGURATION/pxctest $PRODUCTS/$CONFIGURATION/*.framework $DESTINATION/
