48e21aaa1b80c9d67caf9fa49f592b977ad395d4
Johannes Plunien <plu@pqpq.de>
2016-12-09 07:09:54 +0400

    Extract context into properties

--- PXCTestKit/Reporter/ReporterRegistry.swift
+++ PXCTestKit/Reporter/ReporterRegistry.swift
@@ -11,19 +11,23 @@ import Foundation
 
 final class ReporterRegistry {
 
-    private let context: ReporterContext
+    private let consoleOutput: ConsoleOutput
+    private let output: OutputManager
+    private let reporterType: ConsoleReporter.Type
     private var reporters: [ConsoleReporter] = []
 
     init(context: ReporterContext) {
-        self.context = context
+        consoleOutput = context.consoleOutput
+        output = context.output
+        reporterType = context.reporterType
     }
 
     func addReporter(for simulator: FBSimulator, target: FBXCTestRunTarget) throws -> FBTestManagerTestReporter {
         let simulatorIdentifier = "\(simulator.configuration!.deviceName) \(simulator.configuration!.osVersionString)"
-        let consoleReporter = context.reporterType.init(simulatorIdentifier: simulatorIdentifier, testTargetName: target.name, consoleOutput: context.consoleOutput)
-        let junitReportURL = context.output.urlFor(simulatorConfiguration: simulator.configuration!, target: target.name).appendingPathComponent("junit.xml")
+        let consoleReporter = reporterType.init(simulatorIdentifier: simulatorIdentifier, testTargetName: target.name, consoleOutput: consoleOutput)
+        let junitReportURL = output.urlFor(simulatorConfiguration: simulator.configuration!, target: target.name).appendingPathComponent("junit.xml")
         let junitReporter = FBTestManagerTestReporterJUnit.withOutputFileURL(junitReportURL)
-        let xcodeReportURL = context.output.urlFor(simulatorConfiguration: simulator.configuration!, target: target.name).appendingPathComponent("test.log")
+        let xcodeReportURL = output.urlFor(simulatorConfiguration: simulator.configuration!, target: target.name).appendingPathComponent("test.log")
         let xcodeReporter = try XcodeReporter(fileURL: xcodeReportURL)
 
         reporters.append(consoleReporter)
@@ -32,7 +36,7 @@ final class ReporterRegistry {
     }
 
     func finishReporting() throws {
-        try context.reporterType.finishReporting(reporters: reporters)
+        try reporterType.finishReporting(reporters: reporters)
     }
 
 }
